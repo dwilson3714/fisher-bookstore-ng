258fbc48f566d439a148293cb62bc583e6e13880
Dylan Wilson <dmw3714@gmail.com>
2018-02-21 18:56:23 -0500

    Added routing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { HomeComponent } from './home/home.component';
+import { OrdersComponent } from './orders/orders.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRouterModule } from './router/router.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    BookDetailComponent,
+    HomeComponent,
+    OrdersComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
+
+  
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -9,11 +9,15 @@ import { BOOKS } from '../books/mock-books-service'
 })
 export class BooksComponent implements OnInit {
   books = BOOKS;
-
+  selectedBook: Book;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(book: Book): void{
+    this.selectedBook = book;
   }
 
 }
